957eedcb04802ff90d289d6f4911ea139348190a
Jose Maria Samos <jsamos@cifpfbmoll.eu>
2020-05-04 20:37:26 +0200

    feat: Añadir clase Controlador con metodo nombre
    Añadir clase con la función de un controlador spring y
    el método nombre que devuelve la cantidad de personas en
    la base de datos

---
+++ src/main/java/org/formacio/mvc/Controlador.java
@@ -0,0 +1,21 @@
+package org.formacio.mvc;
+
+import org.formacio.repositori.AgendaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class Controlador {
+
+	@Autowired
+	AgendaService agenda;
+	
+	@RequestMapping(path = "/nombre")
+	@ResponseBody
+	public String nombre() {
+		return Integer.toString(agenda.nombreContactes());
+	}
+	
+}
