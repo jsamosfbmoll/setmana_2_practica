7805dcb1f72a9cf66bc2716c45cd925fc71ac547
Jose Maria Samos <jsamos@cifpfbmoll.eu>
2020-05-04 20:41:30 +0200

    feat: Añadir método telefon en Controlador
    Añadir método telefon que devuelve el teléfono
    de la persona con el id especificado

--- src/main/java/org/formacio/mvc/Controlador.java
+++ src/main/java/org/formacio/mvc/Controlador.java
@@ -3,6 +3,7 @@ package org.formacio.mvc;
 import org.formacio.repositori.AgendaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -16,6 +17,12 @@ public class Controlador {
 	@ResponseBody
 	public String nombre() {
 		return Integer.toString(agenda.nombreContactes());
+	}
+	
+	@RequestMapping(path = "/telefon")
+	@ResponseBody
+	public String telefon(String id) {
+		return agenda.recupera(id).getTelefon();
 	}
 	
 }
