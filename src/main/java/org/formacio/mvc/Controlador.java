a5dc298897dac336bd6b8ac6f7dd6d7254cb4751
Jose Maria Samos <jsamos@cifpfbmoll.eu>
2020-05-04 21:40:13 +0200

    feat: Añadir control de error 404
    Añadir control de la excepción 404 (no encontrado) que se
    devuelve si la persona que se busca no existe en la base de datos

--- src/main/java/org/formacio/mvc/Controlador.java
+++ src/main/java/org/formacio/mvc/Controlador.java
@@ -3,7 +3,9 @@ package org.formacio.mvc;
 import org.formacio.repositori.AgendaService;
 import org.formacio.repositori.Persona;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,9 +30,12 @@ public class Controlador {
 	}
 	
 	@RequestMapping(path = "/contacte/{id}", produces = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE})
-	@ResponseBody
-	public Persona contacte(@PathVariable String id) {
-		return agenda.recupera(id);
+	public ResponseEntity<Persona> contacte(@PathVariable String id) {
+		Persona persona = agenda.recupera(id);
+		
+		if (persona == null) return new ResponseEntity<Persona>(persona, HttpStatus.NOT_FOUND);
+		
+		return new ResponseEntity<Persona>(persona, HttpStatus.OK);
 	}
 	
 }
